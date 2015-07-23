296cb0d54bec4ac1a86560cb0d14bc0a6129dce1
Matthew Torres <gr8saxo@gr8saxo.com>
2015-07-23 18:06:33 +0000

    Add component single.json

---
+++ ui/single.json.reel/single.json.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/single.json.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Single.json
+ * @extends Component
+ */
+exports.Single.json = Component.specialize(/** @lends Single.json# */ {
+    constructor: {
+        value: function Single.json() {
+            this.super();
+        }
+    }
+});
